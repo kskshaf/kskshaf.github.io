05c139f2256bc9bb7355792022d512e1a86340bf
kskshaf <118548920+kskshaf@users.noreply.github.com>
2024-05-13 22:18:55 +0800

    Site updated: 2024-05-13 22:18:55

--- js/music-2.js
+++ js/music-2.js
@@ -12,6 +12,55 @@ const musicpages = new APlayer({
     listFolded: false,
     audio: [
     {
+        name: "绽放",
+        artist: '戴娆',
+        url: 'https://registry.npmmirror.com/lokocdnms-pkg-ch01/1.0.0/files/01.m4a',
+        cover: 'https://image2.sina.com.cn/ent/y/d/2006-08-01/U996P28T3D1181799F329DT20060801172912.jpg',
+        // lrc: '01.lrc',
+        theme: '#F695DE'
+    }, {
+        name: "你曾是少年",
+        artist: 'S.H.E',
+        url: 'https://registry.npmmirror.com/lokocdnms-pkg-ch01/1.0.0/files/02.m4a',
+        cover: 'https://registry.npmmirror.com/lokocdnms-pkg-ch01/1.0.0/files/02.webp',
+        // lrc: '02.lrc',
+        theme: '#A4A4A4'
+    }, {
+        name: "最初的梦想",
+        artist: '范玮琪',
+        url: 'https://registry.npmmirror.com/lokocdnms-pkg-ch01/1.0.0/files/03.m4a',
+        cover: 'https://registry.npmmirror.com/lokocdnms-pkg-ch01/1.0.0/files/03.webp',
+        // lrc: '03.lrc',
+        theme: '#FFCE7B'
+    }, {
+        name: "流星雨",
+        artist: 'F4',
+        url: 'https://registry.npmmirror.com/lokocdnms-pkg-ch01/1.0.0/files/04.m4a',
+        cover: 'https://registry.npmmirror.com/lokocdnms-pkg-ch01/1.0.0/files/04.webp',
+        // lrc: '04.lrc',
+        theme: '#89D2FC'
+    }, {
+        name: "喜欢你",
+        artist: 'G.E.M.邓紫棋',
+        url: 'https://registry.npmmirror.com/lokocdnms-pkg-ch01/1.0.0/files/05.mp3',
+        cover: 'https://registry.npmmirror.com/lokocdnms-pkg-ch01/1.0.0/files/05.jpg',
+        // lrc: '05.lrc',
+        theme: '#A6D1E3'
+    }, {
+        name: "敢爱敢做",
+        artist: '林子祥',
+        url: 'https://registry.npmmirror.com/lokocdnms-pkg-ch01/1.0.0/files/06.m4a', //https://youtu.be/HGmTVMZR0hE
+        cover: 'https://registry.npmmirror.com/lokocdnms-pkg-ch01/1.0.0/files/06.webp',
+        // lrc: '06.lrc',
+        theme: '#EE8463'
+    }, {
+        name: "真的汉子",
+        artist: '林子祥',
+        url: 'https://registry.npmmirror.com/lokocdnms-pkg-ch01/1.0.0/files/07.m4a', //https://youtu.be/l4qCBKuS0Bc
+        cover: 'https://registry.npmmirror.com/lokocdnms-pkg-ch01/1.0.0/files/07.webp',
+        // lrc: '07.lrc',
+        theme: '#74BEFD'
+    }, {
         name: "life",
         artist: '霜月はるか',
         url: 'https://registry.npmmirror.com/lokocdnms-pkg-04/1.0.2/files/shimotsukin/KDSD-20017/disc1/07.m4a',
@@ -84,7 +133,7 @@ const musicpages = new APlayer({
     },  {
         name: "さくら、Reincarnation - Arrange",
         artist: '忍',
-        url: 'https://unpkg.com/moyu-cdnms-pkg-04@1.0.0/14.m4a',
+        url: 'https://registry.npmmirror.com/moyu-cdnms-pkg-04/1.0.0/files/14.m4a',
         cover: 'https://registry.npmmirror.com/lokocdnms-pkg-exlrc/1.0.5/files/sakura-moyu/cover.webp',
         theme: '#FE8FA7'
     },
