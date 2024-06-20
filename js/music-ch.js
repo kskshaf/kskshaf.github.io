d0ff55a3a37132d4e0733e5ffd517162868dbc2c
kskshaf <118548920+kskshaf@users.noreply.github.com>
2024-06-20 20:01:12 +0800

    Site updated: 2024-06-20 20:01:12

--- js/music-ch.js
+++ js/music-ch.js
@@ -14,51 +14,65 @@ const chmusic = new APlayer({
     {
         name: "绽放",
         artist: '戴娆',
-        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/01.m4a',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/01.m4a',
         cover: 'https://image2.sina.com.cn/ent/y/d/2006-08-01/U996P28T3D1181799F329DT20060801172912.jpg',
-        // lrc: '01.lrc',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/01.lrc',
         theme: '#F695DE'
     }, {
         name: "你曾是少年",
         artist: 'S.H.E',
-        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/02.m4a',
-        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/02.webp',
-        // lrc: '02.lrc',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/02.m4a',
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/02.webp',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/02.lrc',
         theme: '#A4A4A4'
     }, {
         name: "最初的梦想",
         artist: '范玮琪',
-        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/03.m4a',
-        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/03.webp',
-        // lrc: '03.lrc',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/03.m4a',
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/03.webp',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/03.lrc',
         theme: '#FFCE7B'
     }, {
         name: "流星雨",
         artist: 'F4',
-        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/04.m4a',
-        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/04.webp',
-        // lrc: '04.lrc',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/04.m4a',
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/04.webp',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/04.lrc',
         theme: '#89D2FC'
     }, {
         name: "喜欢你",
         artist: 'G.E.M.邓紫棋',
-        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/05.mp3',
-        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/05.jpg',
-        // lrc: '05.lrc',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/05.mp3',
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/05.jpg',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/05.lrc',
         theme: '#A6D1E3'
     }, {
         name: "敢爱敢做",
         artist: '林子祥',
-        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/06.m4a', //https://youtu.be/HGmTVMZR0hE
-        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/06.webp',
-        // lrc: '06.lrc',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/06.m4a', //https://youtu.be/HGmTVMZR0hE
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/06.webp',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/06.lrc',
         theme: '#EE8463'
     }, {
         name: "真的汉子",
         artist: '林子祥',
-        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/07.m4a', //https://youtu.be/l4qCBKuS0Bc
-        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/07.webp',
-        // lrc: '07.lrc',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/07.m4a', //https://youtu.be/l4qCBKuS0Bc
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/07.webp',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/07.lrc',
+        theme: '#74BEFD'
+    }, {
+        name: "街头霸王榜",
+        artist: '林子祥',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/08.m4a', //https://youtu.be/wE_bPQ29PFE
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/08.webp',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/08.lrc',
+        theme: '#74BEFD'
+    }, {
+        name: "十分十二寸",
+        artist: '林子祥',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/09.m4a', //https://youtu.be/ua9R7rjo4LM
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/09.webp',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/09.lrc',
         theme: '#74BEFD'
     }
     ]
--- js/music.js
+++ js/music.js
@@ -11,55 +11,106 @@ const apall = new APlayer({
     lrcType: 3,
     listFolded: false,
     audio: [
+    // wonderful everyday
+    {
+        name: "空気力学少女と少年の詩 -Piano Ver.-",
+        artist: 'szak',
+        url: 'https://unpkg.com/lokocdnms-pkg-01@1.0.1/wonderful_everyday/disc2-17.m4a',
+        theme: '#9F604C'
+    }, {
+        name: "夜の向日葵",
+        artist: 'szak',
+        url: 'https://unpkg.com/lokocdnms-pkg-01@1.0.1/wonderful_everyday/disc1-2.m4a',
+        theme: '#9F604C'
+    }, {
+        name: "変わらないからこそ",
+        artist: 'szak',
+        url: 'https://unpkg.com/lokocdnms-pkg-01@1.0.1/wonderful_everyday/disc1-3.m4a',
+        theme: '#9F604C'
+    }, {
+        name: "音に出来る事",
+        artist: 'H.B STUDIO',
+        url: 'https://unpkg.com/lokocdnms-pkg-01@1.0.1/wonderful_everyday/disc1-1.m4a',
+        theme: '#9F604C'
+    }, {
+        name: "夏の大三角",
+        artist: 'ryo',
+        url: 'https://unpkg.com/lokocdnms-pkg-01@1.0.1/wonderful_everyday/disc1-20.m4a',
+        theme: '#9F604C'
+    }, {
+        name: "同じ空はどこにも無い",
+        artist: "H.B STUDIO",
+        url: 'https://unpkg.com/lokocdnms-pkg-01@1.0.1/wonderful_everyday/disc2-12.m4a',
+        theme: '#9F604C'
+    }, {
+        name: "窓と光",
+        artist: 'szak',
+        url: 'https://unpkg.com/lokocdnms-pkg-01@1.0.1/wonderful_everyday/disc2-13.m4a',
+        theme: '#9F604C'
+    },
     // new songs here
     {
         name: "绽放",
         artist: '戴娆',
-        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/01.m4a',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/01.m4a',
         cover: 'https://image2.sina.com.cn/ent/y/d/2006-08-01/U996P28T3D1181799F329DT20060801172912.jpg',
-        // lrc: '01.lrc',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/01.lrc',
         theme: '#F695DE'
     }, {
         name: "你曾是少年",
         artist: 'S.H.E',
-        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/02.m4a',
-        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/02.webp',
-        // lrc: '02.lrc',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/02.m4a',
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/02.webp',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/02.lrc',
         theme: '#A4A4A4'
     }, {
         name: "最初的梦想",
         artist: '范玮琪',
-        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/03.m4a',
-        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/03.webp',
-        // lrc: '03.lrc',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/03.m4a',
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/03.webp',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/03.lrc',
         theme: '#FFCE7B'
     }, {
         name: "流星雨",
         artist: 'F4',
-        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/04.m4a',
-        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/04.webp',
-        // lrc: '04.lrc',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/04.m4a',
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/04.webp',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/04.lrc',
         theme: '#89D2FC'
     }, {
         name: "喜欢你",
         artist: 'G.E.M.邓紫棋',
-        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/05.mp3',
-        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/05.jpg',
-        // lrc: '05.lrc',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/05.mp3',
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/05.jpg',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/05.lrc',
         theme: '#A6D1E3'
     }, {
         name: "敢爱敢做",
         artist: '林子祥',
-        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/06.m4a', //https://youtu.be/HGmTVMZR0hE
-        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/06.webp',
-        // lrc: '06.lrc',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/06.m4a', //https://youtu.be/HGmTVMZR0hE
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/06.webp',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/06.lrc',
         theme: '#EE8463'
     }, {
         name: "真的汉子",
         artist: '林子祥',
-        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/07.m4a', //https://youtu.be/l4qCBKuS0Bc
-        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.0/07.webp',
-        // lrc: '07.lrc',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/07.m4a', //https://youtu.be/l4qCBKuS0Bc
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/07.webp',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/07.lrc',
+        theme: '#74BEFD'
+    }, {
+        name: "街头霸王榜",
+        artist: '林子祥',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/08.m4a', //https://youtu.be/wE_bPQ29PFE
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/08.webp',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/08.lrc',
+        theme: '#74BEFD'
+    }, {
+        name: "十分十二寸",
+        artist: '林子祥',
+        url: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/09.m4a', //https://youtu.be/ua9R7rjo4LM
+        cover: 'https://unpkg.com/lokocdnms-pkg-ch01@1.0.1/09.webp',
+        lrc: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.6/ch/09.lrc',
         theme: '#74BEFD'
     }, {
         name: "life",
@@ -137,43 +188,6 @@ const apall = new APlayer({
         url: 'https://unpkg.com/moyu-cdnms-pkg-04@1.0.0/14.m4a',
         cover: 'https://unpkg.com/lokocdnms-pkg-exlrc@1.0.5/sakura-moyu/cover.webp',
         theme: '#FE8FA7'
-    },
-    // wonderful everyday
-    {
-        name: "空気力学少女と少年の詩 -Piano Ver.-",
-        artist: 'szak',
-        url: 'https://unpkg.com/lokocdnms-pkg-01@1.0.1/wonderful_everyday/disc2-17.m4a',
-        theme: '#9F604C'
-    }, {
-        name: "夜の向日葵",
-        artist: 'szak',
-        url: 'https://unpkg.com/lokocdnms-pkg-01@1.0.1/wonderful_everyday/disc1-2.m4a',
-        theme: '#9F604C'
-    }, {
-        name: "変わらないからこそ",
-        artist: 'szak',
-        url: 'https://unpkg.com/lokocdnms-pkg-01@1.0.1/wonderful_everyday/disc1-3.m4a',
-        theme: '#9F604C'
-    }, {
-        name: "音に出来る事",
-        artist: 'H.B STUDIO',
-        url: 'https://unpkg.com/lokocdnms-pkg-01@1.0.1/wonderful_everyday/disc1-1.m4a',
-        theme: '#9F604C'
-    }, {
-        name: "夏の大三角",
-        artist: 'ryo',
-        url: 'https://unpkg.com/lokocdnms-pkg-01@1.0.1/wonderful_everyday/disc1-20.m4a',
-        theme: '#9F604C'
-    }, {
-        name: "同じ空はどこにも無い",
-        artist: "H.B STUDIO",
-        url: 'https://unpkg.com/lokocdnms-pkg-01@1.0.1/wonderful_everyday/disc2-12.m4a',
-        theme: '#9F604C'
-    }, {
-        name: "窓と光",
-        artist: 'szak',
-        url: 'https://unpkg.com/lokocdnms-pkg-01@1.0.1/wonderful_everyday/disc2-13.m4a',
-        theme: '#9F604C'
     },
     // Others
     {
